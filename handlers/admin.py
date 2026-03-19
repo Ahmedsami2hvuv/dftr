0adc81948a6c0a5217be4cae83abbbf13a8ee258
Ahmedsami2hvuv <reozaki.1211@gmail.com>
2026-03-19 14:57:23 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -3,7 +3,7 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 from database import SessionLocal
-from models import User, LedgerEntry, Debt
+from app_models import User, LedgerEntry, Debt
 from config import ADMIN_ID
 
 
